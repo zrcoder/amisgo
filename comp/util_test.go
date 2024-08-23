81250d52d04af7c1c66b8862aab5ee4ac5b86abc
zhangrangding <love-nankai@163.com>
2024-08-23 15:49:27 +0800

    add  ui for getInnerApiID()

---
+++ comp/util_test.go
@@ -0,0 +1,12 @@
+package comp
+
+import "testing"
+
+func Test_getInnerApiID(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		x := getInnerApiID()
+		if x != i {
+			t.Errorf("got %d, want %d", x, i)
+		}
+	}
+}
